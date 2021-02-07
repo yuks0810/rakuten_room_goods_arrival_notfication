6dc5768b23bc8080058f328b4e7cebbee8071cf1
yuki.nakata <dorcushopeino1@gmail.com>
2021-02-07 10:59:52 +0900

    #

--- main.py
+++ main.py
@@ -6,17 +6,35 @@ from bs4 import BeautifulSoup
 import smtplib
 from email.mime.text import MIMEText
 
-kawase_url = "https://www.nikkei.com/markets/kawase/"
+# kawase_url = "https://www.nikkei.com/markets/kawase/"
+ITEM_URL = "https://item.rakuten.co.jp/finebookpremiere/10003220/?gclid=Cj0KCQiAmfmABhCHARIsACwPRADAlAREiKuQHirblXntpVMoxCAL7Nj993L8Z1DyzqfZYVZezcB5LBYaAir4EALw_wcB&scid=af_pc_etc&sc2id=af_113_0_10001868&icm_acid=288-622-7470&icm_agid=58708966751&icm_cid=1424232125&gclid=Cj0KCQiAmfmABhCHARIsACwPRADAlAREiKuQHirblXntpVMoxCAL7Nj993L8Z1DyzqfZYVZezcB5LBYaAir4EALw_wcB_"
 
 my_addr = "dorcushopeino1@gmail.com"
 my_pass = "rwqmoyiqmrvgvrlp"
 
-# 為替の取得
-def get_kawase_price():
-    r = requests.get(kawase_url)
+# アイテムの個数を取得
+def get_item_quantity():
+    r = requests.get(ITEM_URL)
     soup = BeautifulSoup(r.content, "html.parser")
-    price_elems = soup.select("div.mkc-stock_prices")
-    return price_elems[0].getText()
+
+    # itemにhtml要素を読み込み、個数が１以上であれば売切れでないと判断
+    item = soup.find('input', class_='rItemUnits')
+    item_value = item['value']
+
+    floating_item_wrapper = soup.find('div', class_='floating-cart-wrapper')
+    item_name = floating_item_wrapper.span.text
+
+    if item_value >= 1:
+        return {"bool": True, "item_name": item_name}
+    else:
+        return {"bool": False, "item_name": item_name}
+
+# 為替の取得
+# def get_kawase_price():
+#     r = requests.get(kawase_url)
+#     soup = BeautifulSoup(r.content, "html.parser")
+#     price_elems = soup.select("div.mkc-stock_prices")
+#     return price_elems[0].getText()
 
 # メッセージの作成
 def create_message(from_addr, to_addr, subject, body_txt):
@@ -36,11 +54,13 @@ def send_mail(msg):
         server.send_message(msg)
 
 def main(event, context):
-    price = get_kawase_price()
-    if price:
-        msg = create_message(my_addr, my_addr, "ドル円レートお知らせ", price)
-        send_mail(msg)
+    item_presence = get_item_quantity()
+    if item_presence['bool'] is True:
+        msg = create_message(my_addr, my_addr, "商品名のお知らせ", item_presence['item_name'])
+    if item_presence['bool'] is False:
+        msg = create_message(my_addr, my_addr, "売り切れのお知らせ", item_presence['item_name'])
 
+    send_mail(msg)
 
 # import classes
 # from browser_controll import BrowserControll
