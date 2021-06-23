b9e7c9cc2d1959ea572ed4aa58c2fbeecee7bf39
Yuki Nakata <nakata.yuki@plus-zero.co.jp>
2021-06-23 20:47:34 +0900

    - set log lotation - removed unneccesarry code from Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,13 @@ WORKDIR $APP_ROOT
 
 COPY . $APP_ROOT
 
-RUN apt-get update && apt-get install -y unzip mariadb-client
+RUN apt-get update && apt-get install -y unzip mariadb-client gcc libmariadb-dev
 
 # pipインストール(最新版)
 RUN curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py \
     && python get-pip.py
 
 RUN pip install pipenv
-# RUN pipenv lock -r > requirements.txt
 RUN pip install -r requirements.txt
 RUN pip install webdriver-manager
 
--- logs/logger.py
+++ logs/logger.py
@@ -1,11 +1,16 @@
 import logging
+import logging.handlers
 
 def setup_logger(name, logfile='access.log'):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
     # create file handler which logs even DEBUG messages
-    fh = logging.FileHandler(logfile)
+    # fh = logging.FileHandler(logfile)
+    
+    # log lotation
+    fh = logging.handlers.RotatingFileHandler(logfile, maxBytes=100000, backupCount=10)
+    # set log level
     fh.setLevel(logging.DEBUG)
     fh_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(name)s - %(funcName)s - %(message)s')
     fh.setFormatter(fh_formatter)
